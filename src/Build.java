0fecd709f3545defd4e365f31797df5f4b2fa81c
jmakho01 <210610870+jmakho01@users.noreply.github.com>
2026-04-15 20:33:12 -0700

    Finished canReach() method, passes all tests

--- src/Build.java
+++ src/Build.java
@@ -74,6 +74,14 @@ public class Build {
    * @return true if the destination is reachable from the start, false otherwise
    */
   public static boolean canReach(Airport start, Airport destination) {
+    return canReach(start, destination, new HashSet<>());
+  }
+
+  public static boolean canReach(Airport start, Airport destination, Set<Airport> seen) {
+    if(start == destination) return true;
+    if(start == null || seen.contains(start)) return false;
+    seen.add(start);
+    for(Airport connecting : start.getOutboundFlights()) { if(canReach(connecting, destination, seen)) return true; }
     return false;
   }
 
